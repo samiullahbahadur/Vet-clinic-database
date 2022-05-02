3ad084605638c2ebde0b1136dd9a8db18b9a2ae7
samiullahbahadur <bahadursamiullah@gmail.com>
2022-05-03 00:23:13 +0430

    Create insert queri

---
+++ data.sql
@@ -0,0 +1,10 @@
+/* Populate database with sample data. */
+INSERT INTO
+  animals (
+    NAME,
+    date_of_birth,
+    escape_attempts,
+    neutered,
+    weight_kg
+  )
+VALUES ('Agumon', 'February 3, 2020', 0, TRUE, 10.23);
