7826faab61010cfc95a39cd89fbca40e4b237d8e
samiullahbahadur <bahadursamiullah@gmail.com>
2022-05-03 00:24:09 +0430

    Complete insert queri

--- data.sql
+++ data.sql
@@ -7,4 +7,32 @@ INSERT INTO
     neutered,
     weight_kg
   )
-VALUES ('Agumon', 'February 3, 2020', 0, TRUE, 10.23);
+VALUES ('Agumon', 'February 3, 2020', 0, TRUE, 10.23);INSERT INTO
+  animals (
+    NAME,
+    date_of_birth,
+    escape_attempts,
+    neutered,
+    weight_kg
+  )
+VALUES ('Gabumon', 'November 15, 2018', 2, TRUE, 8);
+
+INSERT INTO
+  animals (
+    NAME,
+    date_of_birth,
+    escape_attempts,
+    neutered,
+    weight_kg
+  )
+VALUES ('Pikachu', 'January 7, 2021', 1, FALSE, 15.04);
+
+INSERT INTO
+  animals (
+    NAME,
+    date_of_birth,
+    escape_attempts,
+    neutered,
+    weight_kg
+  )
+VALUES ('Devimon', 'May 12, 2017', 5, TRUE, 11);
