5b4d5ecefc4eb540dad1ac6b8989e6a3018413de
samiullahbahadur <bahadursamiullah@gmail.com>
2022-05-03 00:32:01 +0430

    Add select queri  NAME IN ('Agumon', 'Pikachu')

--- queries.sql
+++ queries.sql
@@ -7,7 +7,12 @@ SELECT *
 FROM animals
 WHERE
   date_of_birth BETWEEN 'January 1, 2016' AND 'December 31, 2019';
-  
+
 SELECT *
 FROM animals
 WHERE neutered = TRUE AND escape_attempts < 3;
+
+
+SELECT date_of_birth
+FROM animals
+WHERE NAME IN ('Agumon', 'Pikachu');
