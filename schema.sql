3122d1ae5c9bdaf786cab9df3c2bf70329f2e659
samiullahbahadur <bahadursamiullah@gmail.com>
2022-05-03 00:21:49 +0430

    Create animals table

--- schema.sql
+++ schema.sql
@@ -2,5 +2,14 @@
 CREATE DATABASE Vet_clinic;
 
 
+CREATE TABLE
+  animals (
+ ID INT PRIMARY KEY	GENERATED ALWAYS AS IDENTITY,
+    NAME VARCHAR(64) NOT NULL,
+    date_of_birth DATE NOT NULL,
+    escape_attempts INT NOT NULL,
+    neutered BOOLEAN NOT NULL,
+    weight_kg REAL  NOT NULL
+  );
 
 
