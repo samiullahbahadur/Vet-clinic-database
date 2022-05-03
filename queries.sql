e9d6801b6bf012246cc427a80f1be112e139acf5
samiullahbahadur <bahadursamiullah@gmail.com>
2022-05-03 21:48:27 +0430

    Update animals use transaction

--- queries.sql
+++ queries.sql
@@ -32,3 +32,10 @@ WHERE NAME <> 'Gabumon';
 SELECT *
 FROM animals
 WHERE weight_kg BETWEEN 10.4 AND 17.3;  
+
+BEGIN;
+UPDATE animals
+SET species = 'unspecified';
+SELECT * FROM animals;
+ROLLBACK;
+SELECT * FROM animals
