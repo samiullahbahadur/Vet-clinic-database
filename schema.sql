d04cb5e20f26a5afa3b4ac4ae68bfc1e0aefcef7
samiullahbahadur <bahadursamiullah@gmail.com>
2022-05-04 21:30:56 +0430

    Add  foreign key  species column in animals

--- schema.sql
+++ schema.sql
@@ -35,3 +35,9 @@ CREATE TABLE
 
 ALTER TABLE animals
 ADD COLUMN species_id INT;
+
+ALTER TABLE animals
+ADD
+  CONSTRAINT species_fk FOREIGN KEY (species_id) REFERENCES species(id)
+  ON
+DELETE CASCADE;
