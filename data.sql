7ea7c537bcc9d4c605d2b873667e3ad8da0db9e4
samiullahbahadur <bahadursamiullah@gmail.com>
2022-05-05 17:56:55 +0430

    Insert in to specializations table

--- data.sql
+++ data.sql
@@ -229,3 +229,48 @@ VALUES ('William Tatcher', 45, 'April 23, 2000'),
 ('Maisy Smith', 26, 'January 17, 2019'),
 ('Stephanie Mendez', 64, 'May 4, 1981'),
 ('Jack Harkness', 38, 'April 8, 2008');
+
+
+INSERT INTO specializations (species_id, vets_id)
+VALUES ( (
+      SELECT id
+      FROM species
+      WHERE NAME = 'Pokemon'
+    ), (
+      SELECT id
+      FROM vets
+      WHERE NAME = 'William Tatcher'
+    )
+
+  ),
+
+  ( (
+      SELECT id
+      FROM species
+      WHERE NAME = 'Digimon'
+    ), (
+      SELECT id
+      FROM vets
+      WHERE NAME = 'Stephanie Mendez'
+    )
+  ),
+  ( (
+      SELECT id
+      FROM species
+      WHERE NAME = 'Pokemon'
+    ), (
+      SELECT id
+      FROM vets
+      WHERE NAME = 'Stephanie Mendez'
+    )
+  ),
+  ( (
+      SELECT id
+      FROM species
+      WHERE NAME = 'Digimon'
+    ), (
+      SELECT id
+      FROM vets
+      WHERE NAME = 'Jack Harkness'
+    )
+  );
