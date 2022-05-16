53258f2c4a9167f7cd6a22f8a32415a4e85ae050
samiullahbahadur <bahadursamiullah@gmail.com>
2022-05-16 21:43:26 +0430

    Add select query to owners  table

--- queries.sql
+++ queries.sql
@@ -123,3 +123,5 @@ WHERE animals.escape_attempts = 0 AND owner_id = (SELECT id FROM owners WHERE fu
 explain analyse SELECT COUNT(*) FROM visits where animals_id=4
 
 SELECT * FROM visits where vet_id = 2;
+
+SELECT * FROM owners where email = 'owner_18327@mail.com';
