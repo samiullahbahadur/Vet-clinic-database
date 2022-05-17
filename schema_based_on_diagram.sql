5a308c21874e4d7a60f673a6c05586ebb5a363b0
samiullahbahadur <bahadursamiullah@gmail.com>
2022-05-17 19:05:15 +0430

    Fix the syntax error

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -1,7 +1,7 @@
 -- create patients table 
 CREATE TABLE patients(
 	id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-	name CHAR(255),
+	name varchar(255),
 	date_of_birth DATE
 );
 
@@ -10,7 +10,7 @@ CREATE TABLE medical_histories(
 	id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
 	admitted_at TIME,
 	patient_id INT ,
-	status CHAR(255),
+	status varchar(255),
     FOREIGN KEY (patient_id) REFERENCES patients(id)
 );
 
@@ -20,24 +20,29 @@ CREATE TABLE invoices(
 	total_amount DECIMAL,
 	generated_at TIME,
 	payed_at TIME,
-	medical_history_id INT FOREIGN KEY REFERENCES medical_histories(id)
+	medical_history_id INT,
+     FOREIGN KEY (medical_history_id ) REFERENCES medical_histories(id)
 );
 
 
 -- create treatments table
 CREATE TABLE treatments(
 	id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-	type CHAR(255),
-	name CHAR(255)
+	type varchar(255),
+	name varchar(255)
 );
 
 
 -- add table relationships
 -- create a "join table" called hospitalizations for the medical_histories and treatments tables
+
 CREATE TABLE hospitalizations(
-    ID int PRIMARY KEY ALWAYS as IDENTITY,
-	medical_history_id INT FOREIGN KEY REFERENCES medical_histories(id),
-	treatment_id INT FOREIGN KEY REFERENCES treatments(id)
+    ID int PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+	medical_history_id INT,
+	treatment_id INT,
+	FOREIGN KEY (treatment_id) REFERENCES treatments(id),
+	
+	FOREIGN KEY (medical_history_id) REFERENCES medical_histories(id)
 );
 
 --create indexes on foreign keys
