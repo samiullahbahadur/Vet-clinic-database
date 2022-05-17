1d3b9442ef92ef2fb2727439b3edcac56a4266fd
Chia Carlyle <chiacarlyle@gmail.com>
2022-05-17 21:18:53 +0100

    Update schema_based_on_diagram.sql
    Create indexes on the foreign keys in the hospitalizations join-table.

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -54,3 +54,5 @@ CREATE INDEX patient_id_asc ON medical_histories(patient_id ASC);
 CREATE INDEX medical_history_id_asc ON invoices(medical_history_id ASC);
 CREATE INDEX invoice_id_asc ON invoice_items(invoice_id ASC);
 CREATE INDEX treatment_id_asc ON invoice_items(treatment_id ASC);
+CREATE INDEX idx_treatment_id ON hospitalizations(treatment_id);
+CREATE INDEX idx_medical_histories_id ON hospitalizations(medical_histories_id);
